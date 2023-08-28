4bc28cde12b3e9f105e28843c7dd1a83a0dacee3
jamebeta <jamebeta@qq.com>
2023-08-28 11:33:02 +0800

    添加dialog添加圆角

--- src/jvmMain/kotlin/components/ConfirmDialog.kt
+++ src/jvmMain/kotlin/components/ConfirmDialog.kt
@@ -1,12 +1,12 @@
 package components
 
+import CustomDialogProvider
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.PopupAlertDialogProvider.AlertDialog
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,7 +22,7 @@ import theme.GOOGLE_RED
 import theme.GOOGLE_YELLOW
 
 
-@OptIn( DelicateCoroutinesApi::class, ExperimentalMaterialApi::class)
+@OptIn(DelicateCoroutinesApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun ConfirmDialog(
     showingDialog: MutableState<Boolean>,
@@ -34,27 +34,34 @@ fun ConfirmDialog(
     runnable: (() -> Unit)? = null,
     list: List<String>? = null,
     width: Int = 320,
-    height: Int = 220,
+    height: Int = 80,
     content: @Composable (() -> Unit) = {
-        val error = mutableStateOf(false)
         Column(
             modifier = Modifier.height(height.dp).width(width.dp).clip(RoundedCornerShape(5.dp))
         ) {
-            Row(modifier = Modifier.fillMaxWidth().padding(5.dp).clip(RoundedCornerShape(5.dp))) {
-                Text(color = titleColor, text = title, fontSize = 16.sp, modifier = Modifier.padding(start = 4.dp))
-            }
             Row(
-                modifier = Modifier.fillMaxWidth().weight(1f).padding(5.dp),
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.fillMaxWidth().weight(1f),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
                 TextField(
                     text.value,
                     onValueChange = { text.value = it },
                     placeholder = { Text(hint) },
-                    isError = error.value,
                     modifier = Modifier.fillMaxWidth()
                 )
             }
+
+        }
+    }
+) {
+    AlertDialog(
+        dialogProvider = CustomDialogProvider,
+        modifier = Modifier.clip(RoundedCornerShape(5.dp)),
+        onDismissRequest = {
+
+        },
+        buttons = {
             Row(
                 verticalAlignment = Alignment.Bottom,
                 horizontalArrangement = Arrangement.End,
@@ -65,13 +72,11 @@ fun ConfirmDialog(
                         var isError = false
                         var errorMsg = ""
                         if (text.value.isBlank()) {
-                            error.value = true
                             isError = true
                             errorMsg = "请正确输入内容"
                         }
                         if (!list.isNullOrEmpty()) {
                             if (list.contains(text.value.trim())) {
-                                error.value = true
                                 errorMsg = "名称已存在"
                                 isError = true
                             }
@@ -93,13 +98,12 @@ fun ConfirmDialog(
                             }
                             return@Button
                         }
-                        error.value = false
                         showingDialog.value = false
                         if (needRun) {
                             runnable!!.invoke()
                         }
                     }, colors = ButtonDefaults.buttonColors(backgroundColor = GOOGLE_BLUE),
-                    modifier = Modifier.padding(end = 10.dp, top = 5.dp, bottom = 5.dp)
+                    modifier = Modifier.padding(end = 3.dp)
                 ) {
                     Text(text = "确定", color = Color.White)
                 }
@@ -108,18 +112,13 @@ fun ConfirmDialog(
                         onClick = {
                             showingDialog.value = false
                         }, colors = ButtonDefaults.buttonColors(backgroundColor = GOOGLE_RED),
-                        modifier = Modifier.padding(end = 10.dp, top = 5.dp, bottom = 5.dp)
+                        modifier = Modifier.padding(end = 3.dp)
                     ) {
                         Text(text = "取消", color = Color.White)
                     }
                 }
             }
-        }
-    }
-) {
-    AlertDialog(onDismissRequest = {
-//        showingDialog.value = false
-    }, content = {
-        content()
-    })
+        },
+        title = { Text(color = titleColor, text = title)},
+        text = { content() })
 }
